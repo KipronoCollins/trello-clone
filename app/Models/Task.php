15d0d5aa0109565e4a832b089ae8cd4ebbc4da93
kipronocollins <kipkirui42@gmail.com>
2022-03-10 14:17:41 +0300

    Models Done

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'category', 'user_id', 'order'];
+
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
